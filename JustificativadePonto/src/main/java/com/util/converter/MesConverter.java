e931050d94a26dbf0a424c629ba984dde2c4f869
celoguimaraes@gmail.com <celoguimaraes@gmail.com@dacabb97-33b7-e89b-86ce-6be5c3f3eaf9>
2012-12-03 00:01:08 +0000

    Mes Converter no relatório da Justificativa

---
+++ JustificativadePonto/src/main/java/com/util/converter/MesConverter.java
@@ -0,0 +1,57 @@
+package com.util.converter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+
+public class MesConverter implements Converter {
+
+	
+	String meses[] = {"janeiro","fevereiro","março", "abril", "maio", "junho", 
+			"julho","agosto", "setembro", "outubro", "novembro", "dezembro"}; 
+	
+
+   
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+    	if(value.trim().equals("")){
+    		return null;
+    	}	
+    		else{
+    			try{
+    				int mes =(Integer.parseInt(value));
+    				return meses[mes];
+    			} catch(NumberFormatException exception) {  
+                    throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Mes Invalido"));  
+                }  
+    	}
+    	
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+    	if (value == null || value.equals("")) {  
+            return "";  
+        } else {  
+        	
+        	try{
+        		
+        		Date dtSelecionada = (Date) value;
+            		
+        		Calendar cal = Calendar.getInstance();
+        		cal.setTime(dtSelecionada);
+        		
+        		int mes = cal.get(Calendar.MONTH); 
+        		return meses[mes];
+			} catch(NumberFormatException exception) {  
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Mes Invalido"));  
+            }  
+        }  
+    }
+
+}
