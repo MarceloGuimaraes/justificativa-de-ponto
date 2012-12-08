6131e363bc50e3438fb4c91e77bdd0974c47f768
rxonda <rxonda@dacabb97-33b7-e89b-86ce-6be5c3f3eaf9>
2012-12-08 19:15:33 +0000

    acerto nos destinatarios do email

--- JustificativadePonto/src/main/java/com/util/JavaMailApp.java
+++ JustificativadePonto/src/main/java/com/util/JavaMailApp.java
@@ -1,71 +1,51 @@
 package com.util;
 
 
+import com.util.mail.JavaMailConf;
 
-import java.util.List;
-
-import javax.mail.Address;
+import javax.mail.*;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-import com.mchange.v1.util.StringTokenizerUtils;
-import com.util.mail.JavaMailConf;
+import java.util.List;
 
 public class JavaMailApp {
 
     private Session session;
 
-    
+
     public JavaMailApp() {
         JavaMailConf confMail = new JavaMailConf();
         session = confMail.Config();
     }
-    
-        
 
-   public void sendMail(String remetente,List<String> destinatarioList,Integer idDoc){
+
+
+    public void sendMail(String remetente, List<String> destinatarios, Integer idDoc){
         try {
 
-            if (remetente == null){
-                remetente = remetente;}
-            
-            String strSubject = new String(); 
-            strSubject ="Enviando email com JavaMail";
-            
-            String corpo = new String();
-            
-            if (corpo == null){
-            	corpo = "Enviei este email utilizando JavaMail com minha conta GMail!";
-            	}
+            String strSubject = "Enviando email com JavaMail";
+
+            String corpo = "Enviei este email utilizando JavaMail com minha conta GMail!";
 
             Message message = new MimeMessage(session);
 
             message.setFrom(new InternetAddress(remetente)); // Remetente
 
-            String strDestinatario = new String();
-           // strDestinatario = // .join(destinatarioList,",");
-            
-         //   strDestinatario= destinatarioList. (destinatarioList, ", ");
-            
-            
-            
-         Address[] toUser = InternetAddress.parse(strDestinatario);
+            Address[] toUser = new Address[destinatarios.size()];
+            int index = 0;
+            for(String destinatario : destinatarios){
+                toUser[index++] = new InternetAddress(destinatario);
+            }
 
-            
-            
             message.setRecipients(Message.RecipientType.TO, toUser);
 
             message.setSubject(strSubject); // Assunto
-            
+
             message.setText(corpo);
-            
+
             /** Metodo para enviar a mensagem criada */
             Transport.send(message);
-
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
