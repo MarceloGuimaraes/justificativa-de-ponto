13c468fa7c5c9b1d9468d06d41be3324e20d35ea
rxonda <rxonda@dacabb97-33b7-e89b-86ce-6be5c3f3eaf9>
2012-12-09 16:58:09 +0000

    Acerto do hide quando motivo eh Faltas ou Falta de Marcacao

--- JustificativadePonto/src/main/java/com/managed/bean/handler/HandlerMotivosManagedBean.java
+++ JustificativadePonto/src/main/java/com/managed/bean/handler/HandlerMotivosManagedBean.java
@@ -7,24 +7,59 @@ import com.model.MotivoEnum;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 import java.io.Serializable;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HandlerMotivosManagedBean implements Serializable {
+    private static final String CAUSA_FALTA = "CAUSA_FALTA";
+    private static final String HORA_INI = "HORA_INI";
+    private static final String HORA_FIM = "HORA_FIM";
     private List<SelectItem> tipoMotivosList;
     private List<SelectItem> tipoFaltaList;
-    private boolean exibeFaltas;
+    private Map<String,Boolean> configuracao;
 
     public HandlerMotivosManagedBean() {
         this.tipoMotivosList = new ComboMotivoDatasourceImpl().findObjects();
         this.tipoFaltaList = new ComboTipoFaltaDatasourceImpl().findObjects();
-        this.exibeFaltas = false;
+        configuracao = new LinkedHashMap<String, Boolean>();
     }
 
     public HandlerMotivosManagedBean(MotivoEnum motivo){
         this();
+        setConfiguracao(motivo);
+    }
+
+    public Map<String, Boolean> getConfiguracao() {
+        return configuracao;
+    }
+
+    private void setConfiguracao(MotivoEnum motivo){
         if(MotivoEnum.FALTAS.equals(motivo)){
-            exibeFaltas = true;
+            setConfiguracaoFaltas();
+        }else if(MotivoEnum.FALTADEMARCACAO.equals(motivo)){
+            setConfiguracaoFaltaMarcacao();
+        }else {
+            setConfiguracaoOutros();
         }
+    }
+
+    private void setConfiguracaoFaltas(){
+        configuracao.put(CAUSA_FALTA, true);
+        configuracao.put(HORA_INI, false);
+        configuracao.put(HORA_FIM, false);
+    }
+
+    private void setConfiguracaoFaltaMarcacao(){
+        configuracao.put(CAUSA_FALTA, false);
+        configuracao.put(HORA_INI, true);
+        configuracao.put(HORA_FIM, false);
+    }
+
+    private void setConfiguracaoOutros(){
+        configuracao.put(CAUSA_FALTA, false);
+        configuracao.put(HORA_INI, true);
+        configuracao.put(HORA_FIM, true);
     }
 
     public List<SelectItem> getTipoMotivosList() {
@@ -44,19 +79,11 @@ public class HandlerMotivosManagedBean implements Serializable {
         this.tipoFaltaList = tipoFaltaList;
     }
 
-    public boolean isExibeFaltas() {
-        return exibeFaltas;
-    }
-
     public void mudouMotivo(ValueChangeEvent e) {
 
         try {
             MotivoEnum novoMotivo = (MotivoEnum) e.getNewValue();
-            if(MotivoEnum.FALTAS.equals(novoMotivo)){
-                exibeFaltas = true;
-            }else{
-                exibeFaltas = false;
-            }
+            setConfiguracao(novoMotivo);
         } catch (ClassCastException cce) {
             throw new RuntimeException("O controlador recebeu um valor inesperado");
         }
