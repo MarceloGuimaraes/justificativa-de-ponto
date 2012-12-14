0d458afba3c06b818cd500940223beabeab989bc
rxonda <rxonda@dacabb97-33b7-e89b-86ce-6be5c3f3eaf9>
2012-12-14 22:47:41 +0000

    Refactoring MailService

--- JustificativadePonto/src/main/java/com/service/mail/JavaMailService.java
+++ JustificativadePonto/src/main/java/com/service/mail/JavaMailService.java
@@ -21,19 +21,6 @@ public class JavaMailService {
 		this.desenv = desenv;
 	}
 
-	/*
-	 * public void sendMail(User remetente, List<User> destinatarios, Integer
-	 * idDoc) {
-	 * 
-	 * List<String> formatados = new LinkedList<String>();
-	 * 
-	 * for (User u : destinatarios) { formatados.add(formata(u)); }
-	 * 
-	 * sendMail(formata(remetente), formatados, idDoc);
-	 * 
-	 * }
-	 */
-
 	private String formata(User user) {
 		StringBuilder endereco = new StringBuilder(user.getNome());
 		endereco.append("<");
@@ -41,6 +28,17 @@ public class JavaMailService {
 		endereco.append(">");
 		return endereco.toString();
 	}
+
+    private List<String> formata(List<User> users){
+        List<String> destinatarios = new LinkedList<String>();
+        if(users==null){
+            return destinatarios;
+        }
+        for (User u : users){
+            destinatarios.add(formata(u));
+        }
+        return destinatarios;
+    }
 
 	public void sendMail(String remetente, List<String> destinatarios,
 			Integer idDoc, String subject, String body) {
@@ -70,85 +68,53 @@ public class JavaMailService {
 	public void enviarCoordenador(User remetente, List<User> destinatarios,
 			Integer idDoc) {
 
-		List<String> formatados = new LinkedList<String>();
-
-		for (User u : destinatarios) {
-			formatados.add(formata(u));
-		}
-
 		assunto = Message
 				.getBundleMessage("mail.teste.subject"); //mail.subject.aguardandoaprovacao.coordenador");
 		corpo = Message.getBundleMessage("mail.teste.corpo"); //mail.corpo.enviocoord");
 
-		sendMail(formata(remetente), formatados, idDoc, assunto, corpo);
+		sendMail(formata(remetente), formata(destinatarios), idDoc, assunto, corpo);
 	}
 
 	public void enviarSuperintendente(User remetente, List<User> destinatarios,
 			Integer idDoc) {
 
-		List<String> formatados = new LinkedList<String>();
-
-		for (User u : destinatarios) {
-			formatados.add(formata(u));
-		}
-
 		assunto = Message
 				.getBundleMessage("mail.subject.aguardandoaprovacao.superintendente");
 		corpo = Message.getBundleMessage("mail.corpo.enviosuper");
 
-		sendMail(formata(remetente), formatados, idDoc, assunto, corpo);
+		sendMail(formata(remetente), formata(destinatarios), idDoc, assunto, corpo);
 	}
 
 	public void enviarRh(User remetente, List<User> destinatarios, Integer idDoc) {
-
-		List<String> formatados = new LinkedList<String>();
-
-		for (User u : destinatarios) {
-			formatados.add(formata(u));
-		}
 
 		assunto = Message
 				.getBundleMessage("mail.subject.aguardandoaprovacao.rh");
 		corpo = Message.getBundleMessage("mail.corpo.enviorh");
 
-		sendMail(formata(remetente), formatados, idDoc, assunto, corpo);
+		sendMail(formata(remetente), formata(destinatarios), idDoc, assunto, corpo);
 	}
 
 	
 	public void concluiRh(User remetente, List<User> destinatarios,
 			Integer idDoc) {
 
-		List<String> formatados = new LinkedList<String>();
-
-		for (User u : destinatarios) {
-			formatados.add(formata(u));
-		}
-
 		assunto = Message
 				.getBundleMessage("mail.subject.aprovado.rh.concluir");
 		corpo = Message.getBundleMessage("mail.corpo.concluirh");
 
-		sendMail(formata(remetente), formatados, idDoc, assunto, corpo);
+		sendMail(formata(remetente), formata(destinatarios), idDoc, assunto, corpo);
 	}
 
 	
 	public void cancelado(User remetente, List<User> destinatarios,
 			Integer idDoc) {
 
-		List<String> formatados = new LinkedList<String>();
-
-		for (User u : destinatarios) {
-			formatados.add(formata(u));
-		}
-
 		assunto = Message
 				.getBundleMessage("mail.subject.cancelado");
 		corpo = Message.getBundleMessage("mail.corpo.cancelado");
 
-		sendMail(formata(remetente), formatados, idDoc, assunto, corpo);
+		sendMail(formata(remetente), formata(destinatarios), idDoc, assunto, corpo);
+
 	}
-	
-	
-	
-	
+
 }
