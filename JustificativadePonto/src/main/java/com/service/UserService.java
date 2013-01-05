aa2aaee4711365460ec084ad3328bf88f325083e
celoguimaraes@gmail.com <celoguimaraes@gmail.com@dacabb97-33b7-e89b-86ce-6be5c3f3eaf9>
2013-01-05 13:04:30 +0000

    Senha com criptografia.

--- JustificativadePonto/src/main/java/com/service/UserService.java
+++ JustificativadePonto/src/main/java/com/service/UserService.java
@@ -1,5 +1,13 @@
 package com.service;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.dozer.Mapper;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.dao.IUserDAO;
 import com.domain.dto.CadastroSenha;
 import com.domain.dto.CadastroUsuario;
@@ -8,147 +16,141 @@ import com.domain.dto.UsuarioLogin;
 import com.domain.dto.exception.BusinessException;
 import com.model.PerfilEnum;
 import com.model.User;
-import org.dozer.Mapper;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
+import com.util.Criptografia;
 
 @Transactional(readOnly = true)
-public class UserService implements IUserService,Serializable {
+public class UserService implements IUserService, Serializable {
 
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+	private IUserDAO dao;
 
-    private IUserDAO dao;
+	private Mapper mapper;
 
-    private Mapper mapper;
+	public UserService(IUserDAO dao, Mapper mapper) {
+		this.dao = dao;
+		this.mapper = mapper;
+	}
 
-    public UserService(IUserDAO dao, Mapper mapper) {
-        this.dao = dao;
-        this.mapper = mapper;
-    }
+	@Transactional(readOnly = false)
+	public void addUser(User user) {
+		dao.adicionar(user); // verifica se existe o ID cadastrado
+	}
 
-    @Transactional(readOnly = false)
-    public void addUser(User user) {
-        dao.adicionar(user); 		// verifica se existe o ID cadastrado
-    }
+	@Override
+	public boolean isExisteUser(CadastroUsuario usuario) {
+		User userAmostra = new User();
+		userAmostra.setCpf(usuario.getCpf());
+		userAmostra.setEmail(usuario.getEmail());
+		return (dao.encontraPorAmostra(userAmostra) != null);
+	}
 
-    @Override
-    public boolean isExisteUser(CadastroUsuario usuario) {
-        User userAmostra = new User();
-        userAmostra.setCpf(usuario.getCpf());
-        userAmostra.setEmail(usuario.getEmail());
-        return (dao.encontraPorAmostra(userAmostra)!=null);
-    }
+	@Override
+	public UsuarioLogado buscaPorLogin(UsuarioLogin usuarioLogin) {
+		User user = new User();
+		user.setEmail(usuarioLogin.getEmail());
+		user.setSenha(Criptografia.encodePassword(usuarioLogin.getSenha()));
+		user = dao.encontraPorAmostra(user);
+		if (user == null) {
+			return null;
+		}
+		return mapper.map(user, UsuarioLogado.class);
+	}
 
-    @Override
-    public UsuarioLogado buscaPorLogin(UsuarioLogin usuarioLogin) {
-        User user = new User();
-        user.setEmail(usuarioLogin.getEmail());
-        user.setSenha(usuarioLogin.getSenha());
-        user = dao.encontraPorAmostra(user);
-        if(user==null){
-            return null;
-        }
-        return mapper.map(user, UsuarioLogado.class);
-    }
+	@Override
+	public List<User> recuperaCoordenadores() {
+		return dao.listar(EnumSet.of(PerfilEnum.COORDENADOR));
+	}
 
-    @Override
-    public List<User> recuperaCoordenadores() {
-        return dao.listar(EnumSet.of(PerfilEnum.COORDENADOR));
-    }
+	@Override
+	public List<User> recuperaSuperintendentes() {
+		return dao.listar(EnumSet.of(PerfilEnum.SUPERINTENDENTE));
+	}
 
-    @Override
-    public List<User> recuperaSuperintendentes() {
-        return dao.listar(EnumSet.of(PerfilEnum.SUPERINTENDENTE));
-    }
+	@Override
+	public List<User> recuperaRH() {
+		return dao.listar(EnumSet.of(PerfilEnum.RH));
+	}
 
-    @Override
-    public List<User> recuperaRH() {
-        return dao.listar(EnumSet.of(PerfilEnum.RH));
-    }
+	@Override
+	@Transactional(readOnly = false)
+	public void alteraSenha(CadastroSenha cadastroSenha) {
 
-    @Override
-    @Transactional(readOnly = false)
-    public void alteraSenha(CadastroSenha cadastroSenha) {
+		User user = dao.recuperar(cadastroSenha.getId());
 
-        User user = dao.recuperar(cadastroSenha.getId());
+		if (user == null) {
+			throw new IllegalArgumentException("Usuário não existe no sistema!");
+		}
 
-        if(user==null){
-            throw new IllegalArgumentException("Usuário não existe no sistema!");
-        }
+		if (!user.getSenha().equals(
+				Criptografia.encodePassword(cadastroSenha.getSenha()))) {
+			throw new BusinessException("login.passw.confirm");
+		}
 
-        if(!user.getSenha().equals(cadastroSenha.getSenha())){
-            throw new BusinessException("login.passw.confirm");
-        }
+		user.setSenha(Criptografia.encodePassword(cadastroSenha.getNovaSenha()));
+	}
 
-        user.setSenha(cadastroSenha.getNovaSenha());
+	@Override
+	public User recuperar(Integer id) {
+		return dao.recuperar(id);
+	}
 
-    }
+	@Transactional(readOnly = false)
+	public void updateUser(User user) {
+		dao.atualizar(user);
+	}
 
-    @Override
-    public User recuperar(Integer id) {
-        return dao.recuperar(id);
-    }
+	@Override
+	@Transactional(readOnly = false)
+	public void atualizar(CadastroUsuario usuario) {
+		User usuarioPersistido = dao.recuperar(usuario.getId());
+		mapper.map(usuario, usuarioPersistido);
+		dao.atualizar(usuarioPersistido);
+	}
 
-    @Transactional(readOnly = false)
-    public void updateUser(User user) {
-        dao.atualizar(user);
-    }
+	@Override
+	@Transactional(readOnly = false)
+	public CadastroUsuario adicionar(CadastroUsuario usuario) {
+		User user = mapper.map(usuario, User.class);
+		Serializable id = dao.adicionar(user);
 
-    @Override
-    @Transactional(readOnly = false)
-    public void atualizar(CadastroUsuario usuario) {
-        User usuarioPersistido = dao.recuperar(usuario.getId());
-        mapper.map(usuario, usuarioPersistido);
-        dao.atualizar(usuarioPersistido);
-    }
+		user.setSenha(Criptografia.encodePassword(getDefaultPassword(user
+				.getCpf())));
 
-    @Override
-    @Transactional(readOnly = false)
-    public CadastroUsuario adicionar(CadastroUsuario usuario) {
-        User user = mapper.map(usuario, User.class);
-        Serializable id = dao.adicionar(user);
+		usuario.setId((Integer) id);
 
-        user.setSenha(getDefaultPassword(user.getCpf()));
+		return usuario;
+	}
 
-        usuario.setId((Integer) id);
+	@Override
+	public CadastroUsuario recuperar(Serializable id) {
+		User user = dao.recuperar(id);
+		CadastroUsuario usuario = mapper.map(user, CadastroUsuario.class);
+		return usuario;
+	}
 
-        return usuario;
-    }
+	@Override
+	@Transactional(readOnly = false)
+	public void apagar(CadastroUsuario usuario) {
+		User user = dao.recuperar(usuario.getId());
+		dao.deletar(user);
+	}
 
-    @Override
-    public CadastroUsuario recuperar(Serializable id) {
-        User user = dao.recuperar(id);
-        CadastroUsuario usuario = mapper.map(user, CadastroUsuario.class);
-        return usuario;
-    }
+	@Override
+	public List<CadastroUsuario> todos() {
+		List<CadastroUsuario> resultado = new ArrayList<CadastroUsuario>(5);
+		List<User> usuarios = dao.todos();
+		for (User u : usuarios) {
+			resultado.add(mapper.map(u, CadastroUsuario.class));
+		}
+		return resultado;
+	}
 
-    @Override
-    @Transactional(readOnly = false)
-    public void apagar(CadastroUsuario usuario) {
-        User user = dao.recuperar(usuario.getId());
-        dao.deletar(user);
-    }
-
-    @Override
-    public List<CadastroUsuario> todos() {
-        List<CadastroUsuario> resultado = new ArrayList<CadastroUsuario>(5);
-        List<User> usuarios = dao.todos();
-        for(User u : usuarios){
-            resultado.add(mapper.map(u, CadastroUsuario.class));
-        }
-        return resultado;
-    }
-
-    /**Senha default
-     * composto pelos 5 primeiros numeros do CPF
-     * */
-    private String getDefaultPassword(String strCpf){
-        return strCpf.replace(".","").replace("-", "").substring(0, 5);
-    }
+	/**
+	 * Senha default composto pelos 5 primeiros numeros do CPF
+	 * */
+	private String getDefaultPassword(String strCpf) {
+		return strCpf.replace(".", "").replace("-", "").substring(0, 5);
+	}
 
 }
